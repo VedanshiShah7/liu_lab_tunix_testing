b5be98337a093045c9ac85efd67967f10422c673
Vedanshi Shah <shah.ve@northeastern.edu>
2025-06-17 06:50:27 -0400

    updates from tunix

--- tunix-main/tunix/sft/peft_trainer.py
+++ tunix-main/tunix/sft/peft_trainer.py
@@ -35,6 +35,7 @@ from tunix.sft import inflight_throttler
 from tunix.sft import metrics_logger
 from tunix.sft import profiler
 from tunix.sft import progress_bar
+from tunix.sft import system_metrics_calculator
 
 _ModelInputT = Dict[str, ArrayLike]
 P = ParamSpec("P")
@@ -91,6 +92,23 @@ class TrainingInput:
   input_mask: jax.Array | np.ndarray
 
 
+def _calculate_global_batch_size(train_example: Any) -> int:
+  if dataclasses.is_dataclass(train_example):
+    attributes = dataclasses.asdict(train_example)
+  else:
+    attributes = vars(train_example)
+
+  for field_value in attributes.values():
+    if isinstance(field_value, (jax.Array, np.ndarray)):
+      # Assume the first array we find has the batch dimension.
+      return field_value.shape[0]
+
+  raise TypeError(
+      "Could not automatically determine batch size. No JAX or NumPy "
+      "array found in the training example."
+  )
+
+
 def is_lora_enabled(model: nnx.Module) -> bool:
   for _, value in nnx.iter_graph(model):
     if isinstance(value, nnx.LoRAParam):
@@ -121,17 +139,50 @@ class PeftTrainer:
     self.loss_fn = _default_loss_fn
     self.eval_loss_fn = _default_loss_fn
     self.gen_model_input_fn = lambda x: x
+    self.checkpoint_manager = checkpoint_manager.CheckpointManager(
+        root_directory=self.config.checkpoint_root_directory,
+        options=self.config.checkpointing_options,
+    )
+    self.metrics_logger = metrics_logger.MetricsLogger(
+        self.config.metrics_logging_options
+    )
+    self.use_external_ckpt_manager = False
+
     self._train_steps = 0
     self._eval_steps = 0
     self._throttler = inflight_throttler.InflightThrottler(
         max_inflight=training_config.max_inflight_computations
     )
-    self._metrics_logger = metrics_logger.MetricsLogger(
-        self.config.metrics_logging_options
-    )
     self._mode: metrics_logger.Mode = metrics_logger.Mode.TRAIN
     self._has_aux = False
     self._pbar = None
+    self._total_model_params = sum(
+        p.size
+        for p in jax.tree_util.tree_leaves(
+            nnx.state(self.model).filter(nnx.Param, nnx.LoRAParam)
+        )
+    )
+
+    self._train_steps = self.checkpoint_manager.maybe_restore(
+        self.model, restore_only_lora_params=self._lora_enabled
+    )
+
+    self._jitted_train_step_fn = None
+    self._jitted_eval_step_fn = None
+    self._prof = profiler.Profiler(
+        initial_step=self._train_steps,
+        max_step=self.config.max_steps,
+        profiler_options=self.config.profiler_options,
+    )
+
+  def clear_jit_cache(self):
+    """Clears the JIT cache of the train and eval step functions.
+
+    This function should be called when the trainer is being reused after
+    overiding the training related states, for example, the loss function.
+    """
+    self._jitted_train_step_fn = None
+    self._jitted_eval_step_fn = None
 
   def with_loss_fn(
       self,
@@ -140,6 +191,7 @@ class PeftTrainer:
       ],
       has_aux: bool = False,
   ):
+    self.clear_jit_cache()
     self.loss_fn = loss_fn
     self.eval_loss_fn = loss_fn
     self._has_aux = has_aux
@@ -160,6 +212,7 @@ class PeftTrainer:
     Returns:
       PeftTrainer.
     """
+    self.clear_jit_cache()
     self.gen_model_input_fn = gen_model_input_fn
     return self
 
@@ -201,14 +254,29 @@ class PeftTrainer:
     return eval_step
 
   def jit_train_and_eval_step(self, skip_jit: bool = False):
+    """Creates and returns the train and eval step functions.
+
+    This function will return the cached ones if available.
+
+    Args:
+      skip_jit: If True, the train and eval step functions will not be JITed.
+
+    Returns:
+      A tuple of train and eval step functions.
+    """
     train_step = self.create_train_step_fn()
     eval_step = self.create_eval_step_fn()
     if skip_jit:
       return train_step, eval_step
     else:
-      return nnx.jit(train_step, donate_argnames=("optimizer",)), nnx.jit(
-          eval_step, donate_argnames=("model",)
-      )
+      if self._jitted_train_step_fn is None:
+        self._jitted_train_step_fn = nnx.jit(
+            train_step, donate_argnames=("optimizer",)
+        )
+        self._jitted_eval_step_fn = nnx.jit(
+            eval_step, donate_argnames=("model",)
+        )
+      return self._jitted_train_step_fn, self._jitted_eval_step_fn
 
   def _shard_input(self, input_data: TrainingInput) -> TrainingInput:
     mesh = pxla.thread_resources.env.physical_mesh
@@ -238,9 +306,16 @@ class PeftTrainer:
     """Override this function for post processing aux data from eval step."""
     pass
 
-  def _log_metrics(self, loss: ArrayLike, step: int | None = None):
-    self._metrics_logger.log("loss", loss, self._mode, step)
-    self._metrics_logger.log("perplexity", jnp.exp(loss), self._mode, step)
+  def _log_metrics(
+      self,
+      loss: ArrayLike,
+      step: int | None = None,
+      tflops: float | None = None,
+  ):
+    self.metrics_logger.log("loss", loss, self._mode, step)
+    self.metrics_logger.log("perplexity", jnp.exp(loss), self._mode, step)
+    if tflops is not None:
+      self.metrics_logger.log("tflops", tflops, self._mode, step)
 
   @contextlib.contextmanager
   def _switch_mode(self, mode: metrics_logger.Mode):
@@ -253,7 +328,7 @@ class PeftTrainer:
 
   @property
   def _tqdm_train_metrics(self) -> list[str] | None:
-    return ["loss", "perplexity"]
+    return ["loss", "perplexity", "tflops"]
 
   @property
   def _tqdm_eval_metrics(self) -> list[str] | None:
@@ -275,36 +350,28 @@ class PeftTrainer:
     mesh = pxla.thread_resources.env.physical_mesh
     logging.info("Training with mesh: %s", mesh)
 
-    ckpt_manager = checkpoint_manager.CheckpointManager(
-        root_directory=self.config.checkpoint_root_directory,
-        options=self.config.checkpointing_options,
-    )
     train_step, eval_step = self.jit_train_and_eval_step(skip_jit)
-
-    self._train_steps = ckpt_manager.maybe_restore(
-        self.model, restore_only_lora_params=self._lora_enabled
-    )
 
     if self.config.max_steps is not None:
       self._pbar = progress_bar.ProgressBar(
-          metrics_logger=self._metrics_logger,
+          metrics_logger=self.metrics_logger,
           initial_steps=self._train_steps,
           max_steps=self.config.max_steps,
       )
-
-    prof = profiler.Profiler(
-        initial_step=self._train_steps,
-        max_step=self.config.max_steps,
-        profiler_options=self.config.profiler_options,
-    )
     with time_measure("Train loop"):
       for index, train_example in enumerate(train_ds):
-        # TODO(mridulsahu): Add support to restore the iterator state instead of
-        # skipping the already trained examples.
-        if index < self._train_steps:
-          # Skip the examples that are already trained.
-          continue
-        prof.maybe_activate(self._train_steps)
+        # TODO(annyan): This is a temporary solution to support the external
+        # checkpoint manager. Because with external checkpoint manager, the
+        # training steps are not reset to 0, so need to skip the check blow. We
+        # need to think through how checkpointing should work with external
+        # checkpoint manager.
+        if not self.use_external_ckpt_manager:
+          # TODO(mridulsahu): Add support to restore the iterator state
+          # instead of skipping the already trained examples.
+          if index < self._train_steps:
+            # Skip the examples that are already trained.
+            continue
+        self._prof.maybe_activate(self._train_steps)
         with jax.profiler.StepTraceAnnotation(
             "train", step_num=self._train_steps
         ):
@@ -323,52 +390,74 @@ class PeftTrainer:
 
           train_example = self._prepare_inputs(train_example)
           train_example = self._shard_input(train_example)
+          global_batch_size = _calculate_global_batch_size(train_example)
 
           self._throttler.wait_for_next()
+          step_start_time = time.perf_counter()
           train_loss, aux = train_step(
               self.model, self.optimizer, train_example
           )
+          step_end_time = time.perf_counter()
+          step_time_delta = step_end_time - step_start_time
+
+          tflops = system_metrics_calculator.tflops(
+              total_model_params=self._total_model_params,
+              global_batch_size=global_batch_size,
+              step_time_delta=step_time_delta,
+          )
+
           self._throttler.add_computation(train_loss)
           self._train_steps += 1
           self._post_process_train_step(aux)
-          self._log_metrics(train_loss, self._train_steps)
+          self._log_metrics(
+              train_loss,
+              self._train_steps,
+              tflops,
+          )
           self._may_update_pbar(self._tqdm_train_metrics, increment_steps=True)
 
           logging.info(
               "Train step %d training loss: %f  - training perplexity: %f",
               self._train_steps,
-              self._metrics_logger.get_metric("loss", "train"),
-              self._metrics_logger.get_metric("perplexity", "train"),
+              self.metrics_logger.get_metric("loss", "train"),
+              self.metrics_logger.get_metric("perplexity", "train"),
           )
 
           # Actual checkpoint frequency is configured by checkpointing_options.
-          ckpt_manager.save(
+          self.checkpoint_manager.save(
               self._train_steps,
               self.model,
               save_only_lora_params=self._lora_enabled,
           )
-          prof.maybe_deactivate(self._train_steps)
+          self._prof.maybe_deactivate(self._train_steps)
 
     self._throttler.wait_for_all()
     # Save the final checkpoint forcefully if not already saved.
-    last_saved_step = ckpt_manager.latest_step()
+    last_saved_step = self.checkpoint_manager.latest_step()
     if last_saved_step is None or last_saved_step < self._train_steps:
-      ckpt_manager.save(
+      self.checkpoint_manager.save(
           self._train_steps,
           self.model,
           save_only_lora_params=self._lora_enabled,
           force=True,
       )
-    ckpt_manager.close()
+    if not self.use_external_ckpt_manager:
+      self.checkpoint_manager.close()
     self.close()
 
+  @property
+  def train_steps(self) -> int:
+    return self._train_steps
+
   def close(self):
-    self._metrics_logger.close()
+    self.metrics_logger.close()
     if self._pbar is not None:
       self._pbar.close()
 
   def _run_eval(
-      self, eval_ds: Iterable[Any], eval_step: Callable[..., Any]
+      self,
+      eval_ds: Iterable[Any],
+      eval_step: Callable[..., Any],
   ) -> None:
     """Runs evaluation loop."""
     with self._switch_mode(metrics_logger.Mode.EVAL):
@@ -387,8 +476,8 @@ class PeftTrainer:
       logging.info(
           "Train step %d eval loss: %f - eval perplexity: %f",
           self._train_steps,
-          self._metrics_logger.get_metric("loss", "eval"),
-          self._metrics_logger.get_metric("perplexity", "eval"),
+          self.metrics_logger.get_metric("loss", "eval"),
+          self.metrics_logger.get_metric("perplexity", "eval"),
       )
 
 
